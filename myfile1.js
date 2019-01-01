9c6dbda060391a6d960af0a55df36b8d8a365f78
CHRISTIAN (ISHMAEL) GYABAN <christiangyaban@hotmail.com>
2019-01-01 12:11:16 +0100

    Update myfile1.js
    added arrow functions

--- myfile1.js
+++ myfile1.js
@@ -1,3 +1,33 @@
+//Understanding Arrow functions
+---------------------------------------------------------------------------------------------------------
+var elements = [
+  'Hydrogen',
+  'Helium',
+  'Lithium',
+  'Beryllium'
+];
+
+elements.map(function(element) { 
+  return element.length; 
+}); // this statement returns the array: [8, 6, 7, 9]
+
+elements.map((element) => {
+  return element.length;
+}); // [8, 6, 7, 9]
+
+elements.map(element => {
+  return element.length;
+}); // [8, 6, 7, 9]
+
+elements.map(element => element.length); // [8, 6, 7, 9]
+
+elements.map(({ "length": lengthFooBArX }) => lengthFooBArX); // [8, 6, 7, 9]
+
+elements.map(({ length }) => length); // [8, 6, 7, 9]
+
+
+
+
 //           https://www.toptal.com/javascript/interview-questions
 
 //------------------------------------------------------------------------------------------------------
