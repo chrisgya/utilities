94af3b7baabd0a5de22cfffc0698a105fa6c4b24
CHRISTIAN (ISHMAEL) GYABAN <christiangyaban@hotmail.com>
2019-11-05 12:31:54 +0100

    Update myfile1.js
    added anagrams

--- myfile1.js
+++ myfile1.js
@@ -1116,3 +1116,38 @@ titleCase("I'm a little tea pot");
 
 
 
+
+-----------------------------------------------------------------------------------------------------
+	// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+function anagrams(stringA, stringB) {
+    if(!stringA || !stringB ){
+        return false;
+    }
+    
+    //remove all non-alphanumeric characters like ., !, spaces
+    stringA=stringA.replace(/\W/g, '').toLowerCase();
+    stringB = stringB.replace(/\W/g, '').toLowerCase();
+
+    if(stringA.length !== stringB.length){
+        return false;
+    }
+    const combined = `${stringA}${stringA}`;
+    return combined.indexOf(stringB) !== -1 ? true: false;
+}
+
+module.exports = anagrams;
+
+
+
+
+
+
